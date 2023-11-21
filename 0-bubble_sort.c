381371e755874ee5a757735c485a0a80f41c73f1
me996 <me996@gmail.com>
2023-11-21 04:00:45 -0800

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -34,10 +34,10 @@ for (j = 0; j < n - 1 - i; j++)
 if (array[j] > array[j + 1])
 {
 swap(array + j, array + j + 1);
+print_array(array,size);
 flag = 1;
 }
 }
-print_array(array,size);
 if (flag == 0)
 break;
 }
